afb1bd2d52851eda0ad554840c26635ff172e622
Charlie Rosenbury <gmail@rosenbury.com>
2014-09-10 10:58:27 -0500

    revert to working wp_uploads_rewrite

--- grunt/tasks/wp_uploads_rewrite.js
+++ grunt/tasks/wp_uploads_rewrite.js
@@ -1,49 +1,44 @@
 module.exports = function(grunt){
 
 	function setRewriteRules(action){
+		var htaccess = new FileSection({
+				filename: grunt.config('helperpress.build_dir') + '/.htaccess',
+				marker: {
+					start: '# BEGIN HelperPress',
+					end: '# END HelperPress'
+				}
+			}),
 
-		var files = this.filesSrc;
+			rewriteContents = [];
 
-		files.forEach(function(htaccessSrc){
-			var htaccess = new FileSection({
-					filename: htaccessSrc,
-					marker: {
-						start: '# BEGIN HelperPress',
-						end: '# END HelperPress'
+		if( typeof action === 'undefined' ){
+			action = 'enable';
+		}
+
+		if(action === 'enable'){
+			var rewriteBase = grunt.config('helperpress.apache.scheme') === 'subdir' ? grunt.config('helperpress.environments.local.wp.theme.slug') : '/',
+				rewriteHost = grunt.config('helperpress.environments.' + grunt.config('helperpress.db_master') + '.home_url');
+
+			rewriteContents = [
+				'RewriteEngine On',
+				'RewriteBase ' + rewriteBase,
+				'RewriteRule ^wp-content/uploads/(.+) http://' + rewriteHost + '/wp-content/uploads/$1 [L]'
+			];
+
+			// update site_config via write_site_config task
+			grunt.config('write_site_config.rewrite_rules', {
+				options: {
+					type: 'local',
+					settings: {
+						uploads_sync: 'rewrite'
 					}
-				}),
+				}
+			});
 
-				rewriteContents = [];
+			grunt.task.run('write_site_config:rewrite_rules');
+		}
 
-			if( typeof action === 'undefined' ){
-				action = 'enable';
-			}
-
-			if(action === 'enable'){
-				var rewriteBase = grunt.config('helperpress.apache.scheme') === 'subdir' ? grunt.config('helperpress.environments.local.wp.theme.slug') : '/',
-					rewriteHost = grunt.config('helperpress.environments.' + grunt.config('helperpress.db_master') + '.home_url');
-
-				rewriteContents = [
-					'RewriteEngine On',
-					'RewriteBase ' + rewriteBase,
-					'RewriteRule ^wp-content/uploads/(.+) http://' + rewriteHost + '/wp-content/uploads/$1 [L]'
-				];
-
-				// update helperpress_config via write_helperpress_config task
-				grunt.config('write_helperpress_config.rewrite_rules', {
-					options: {
-						type: 'local',
-						settings: {
-							uploads_sync: 'rewrite'
-						}
-					}
-				});
-
-				grunt.task.run('write_helperpress_config:rewrite_rules');
-			}
-
-			htaccess.write( rewriteContents );
-		});
+		htaccess.write( rewriteContents );
 	}
 
 	grunt.registerTask( 'wp_uploads_rewrite', 'Enables or disables .htaccess rules that rewrite all wp-uploads URLs to db_master environment.', setRewriteRules);
