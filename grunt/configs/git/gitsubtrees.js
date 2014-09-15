f7df8e09b8a03ec43fdaca60da02b0bccb4ccf74
Charlie Rosenbury <gmail@rosenbury.com>
2014-09-15 07:44:27 -0500

    fixes #13

--- grunt/configs/git/gitsubtrees.js
+++ grunt/configs/git/gitsubtrees.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'40d-sass': {
 		'path': '<%= helperpress.assets_dir %>/_src/sass',
-		'url': 'git@github.com:40Digits/forty-sass.git',
+		'url': 'https://github.com/40Digits/forty-sass.git',
 		'branch': 'master'
 	}
 }
